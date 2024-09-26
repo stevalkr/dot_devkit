5689e44a4ff34ed8763a3f08d86e44a337c3967e
Steve Walker <65963536+etherswangel@users.noreply.github.com>
2024-09-26 14:09:14 +0800

    add proxychains

--- templates/ubuntu/ubuntu.sh
+++ templates/ubuntu/ubuntu.sh
@@ -4,14 +4,17 @@ set -e
 touch ~/.Xauthority
 
 sudo apt update
-sudo apt install -y net-tools curl wget build-essential software-properties-common
-sudo add-apt-repository -y ppa:neovim-ppa/unstable
-sudo apt-add-repository -y ppa:fish-shell/release-3
-sudo apt update && sudo apt upgrade -y
+sudo apt install -y net-tools curl wget build-essential software-properties-common proxychains
+sudo sed -i "$(awk 'NF {last=NR} END {print last}' /etc/proxychains.conf)s/.*/socks5 10.19.138.20 7890/" /etc/proxychains.conf
 
-sudo apt install -y git fzf fish neovim
+sudo proxychains add-apt-repository -y ppa:neovim-ppa/unstable
+sudo proxychains apt-add-repository -y ppa:fish-shell/release-3
+sudo proxychains apt update && sudo apt upgrade -y
+
+sudo proxychains apt install -y git fzf fish neovim
 
 chsh -s /usr/bin/fish
 git clone https://github.com/stevalkr/dot_fish.git ~/.config/fish
 git clone https://github.com/stevalkr/dot_nvim.git ~/.config/nvim
 
+sh <(curl -L https://nixos.org/nix/install) --no-daemon
